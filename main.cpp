64fac1867a2372b0bc2db54422432ca7e21266dc
Daniil <daniel217032001@gmail.com>
2020-11-04 22:24:06 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+#include "windows.h"
 
-#include "libraries/example.h"
+#include "libraries/Company.h"
+#include "libraries/clients/ClientsGenerator.h"
+#include "libraries/products/ProductManager.h"
 
 using std::cout;
 
 int main() {
-    example();
+    srand(time(0));
+    auto company = new Company(1000, 3, 1);
+//    while(!company->isBankrupt()) {
+    for (int i = 0; i < 100; ++i) {
+        ProductManager::setValues();
+        auto clients = ClientsGenerator::generate(rand() % 10);
+        company->trade(clients);
 
+        cout << i << "\n";
+//        Sleep(1000);
+    }
+
+    cout << "Банкрот!";
     return 0;
 }
