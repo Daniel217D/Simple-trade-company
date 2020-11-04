df3599a4a2a60ba6c701e38209825ce1a330ff09
Daniil <daniel217032001@gmail.com>
2020-11-05 01:04:30 +0300

    Update ProductManager.h

--- libraries/products/ProductManager.h
+++ libraries/products/ProductManager.h
@@ -5,19 +5,20 @@
 
 using std::map;
 using Cmp::productType;
+using Cmp::productTypesToString;
 
 class ProductManager {
 private:
-    static map <unsigned int, unsigned int> purchase_price;
-    static map <unsigned int, unsigned int> sale_price;
-    static map <unsigned int, unsigned short int> demand;
+    static map<unsigned int, unsigned int> purchase_price;
+    static map<unsigned int, unsigned int> sale_price;
+    static map<unsigned int, short int> demand;
 
 public:
     static void setValues() {
         for (int i = productType::apple; i <= productType::carrot; i++) {
             demand[i] = rand() % 10 - 2;
-            purchase_price[i] = 10;
-            sale_price[i] = 10 + demand[i];
+            purchase_price[i] = 8 + rand() % 5;
+            sale_price[i] = 8 + demand[i];
         }
     }
 
@@ -32,8 +33,18 @@ public:
     static unsigned short int get_demand(productType type) {
         return demand[type];
     }
+
+    static string toString() {
+        string res = "Товары на рынке:\n";
+
+        for (map<unsigned int, unsigned int>::iterator it = purchase_price.begin(); it != purchase_price.end(); it++) {
+            res += productTypesToString[(productType)it->first] + ": закупка - " + to_string(it->second) + "у.е. | продажа - " + to_string(sale_price[it->first]) + "у.е. | популярность - " + to_string(demand[it->first]) + "\n";
+        }
+
+        return res;
+    }
 };
 
-map <unsigned int, unsigned int> ProductManager::purchase_price = {};
-map <unsigned int, unsigned int> ProductManager::sale_price = {};
-map <unsigned int, unsigned short int> ProductManager::demand = {};
+map<unsigned int, unsigned int> ProductManager::purchase_price = {};
+map<unsigned int, unsigned int> ProductManager::sale_price = {};
+map<unsigned int, short int> ProductManager::demand = {};
