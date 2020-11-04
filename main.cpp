66b4bd33858cd2a97e807c1373d671962a3c32f0
Daniil <daniel217032001@gmail.com>
2020-11-05 01:11:12 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,9 +12,10 @@ int main() {
     srand(time(0));
     unsigned start;
     cin >> start;
+
     auto company = new Company(start, 3, 1);
+
     while(!company->isBankrupt()) {
-//    for (int i = 0; i < 100; ++i) {
         ProductManager::setValues();
         company->restore();
         auto clients = ClientsGenerator::generate(5 + rand() % 5);
