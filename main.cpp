49d3e211ef90508102964cbbe120a54e31128cc5
Daniil <daniel217032001@gmail.com>
2020-11-05 01:04:45 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,20 +5,23 @@
 #include "libraries/clients/ClientsGenerator.h"
 #include "libraries/products/ProductManager.h"
 
+using std::cin;
 using std::cout;
 
 int main() {
     srand(time(0));
-    auto company = new Company(1000, 3, 1);
-//    while(!company->isBankrupt()) {
-    for (int i = 0; i < 2; ++i) {
+    unsigned start;
+    cin >> start;
+    auto company = new Company(start, 3, 1);
+    while(!company->isBankrupt()) {
+//    for (int i = 0; i < 100; ++i) {
         ProductManager::setValues();
         company->restore();
         auto clients = ClientsGenerator::generate(5 + rand() % 5);
         company->trade(clients);
+        cout << company->toString() << ProductManager::toString() << "\n";
         company->cycle();
-        cout << company->toString() << "\n";
-//        Sleep(1000);
+        Sleep(5000);
     }
 
     cout << "Банкрот!";
