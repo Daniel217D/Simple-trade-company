07b781521070484e91b6149173c18559c4577fcf
Daniil <daniel217032001@gmail.com>
2020-11-05 00:21:14 +0300

    Update types.h

--- libraries/helpers/types.h
+++ libraries/helpers/types.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <map>
+#include <string>
+
 namespace Cmp {
     enum dayTime {
         morning, day, evening
@@ -15,5 +18,11 @@ namespace Cmp {
 
     enum productType {
         apple = 0, pear = 1, carrot = 2
+    };
+
+    std::map<productType, std::string> productTypesToString = {
+            {apple, "Яблоки"},
+            {pear, "Груши"},
+            {carrot, "Морковь"},
     };
 }
