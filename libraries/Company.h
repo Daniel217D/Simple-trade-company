58f32c72fc3680d9794a7eecd55b884ca338a88f
Daniil <daniel217032001@gmail.com>
2020-11-05 00:21:07 +0300

    Update Company.h

--- libraries/Company.h
+++ libraries/Company.h
@@ -11,6 +11,7 @@
 using std::vector;
 using std::to_string;
 using Cmp::dayTime;
+using Cmp::clientType;
 
 class Company {
 private:
@@ -18,16 +19,26 @@ private:
     dayTime dt;
     vector<SellerShop> seller_shops;
     vector<SellerStorage> seller_storages;
+
+    void collect_money() {
+        for (auto & shop : seller_shops) {
+            money += shop.collectMoney(dt);
+        }
+
+        for (auto & shop : seller_storages) {
+            money += shop.collectMoney(dt);
+        }
+    }
 public:
     explicit Company(int _money, unsigned int shops_amount, unsigned int storage_amount) {
         money = _money;
 
         for (unsigned int i = 0; i < shops_amount; ++i) {
-            seller_shops.emplace_back(to_string(i));
+            seller_shops.emplace_back(SellerShop(to_string(i + 1)));
         }
         
         for (unsigned int i = 0; i < storage_amount; ++i) {
-            seller_storages.emplace_back(to_string(i));
+            seller_storages.emplace_back(SellerStorage(to_string(i + 1)));
         }
     }
 
@@ -35,21 +46,37 @@ public:
         return money;
     }
 
-    void trade(vector<Client> clients) {
+    void trade(const vector<Client>& clients) {
         for (const auto & client : clients) {
             trade(client);
         }
     }
 
     void trade(Client client) {
+        if(client.get_type() == clientType::wholesale) {
+            seller_shops[random() % seller_shops.size()].buy(client);
+        } else {
+            seller_storages[random() % seller_storages.size()].buy(client);
+        }
+    }
 
+    void restore() {
+        for (auto & shop : seller_shops) {
+            shop.restore();
+        }
+
+        for (auto & shop : seller_storages) {
+            shop.restore();
+        }
     }
 
     bool isBankrupt() {
         return money <= 0;
     }
 
-    void changeTime() {
+    void cycle() {
+        collect_money();
+
         if(dt == dayTime::morning) {
             dt = dayTime::day;
         } else if(dt == dayTime::day) {
@@ -57,6 +84,19 @@ public:
         } else if(dt == dayTime::evening) {
             dt = dayTime::morning;
         }
+    }
+
+    string toString() {
+        string res;
+        res += "Бюджет: " + to_string(money) + "\n";
+        for (auto & shop : seller_shops) {
+            res += shop.toString() + "\n";
+        }
+
+        for (auto & shop : seller_storages) {
+            res += shop.toString() + "\n";
+        }
+        return res;
     }
 };
 
